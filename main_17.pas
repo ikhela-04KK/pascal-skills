72865589cd33f3bb120b451ee098fb3842d7bb8c
ikhela-04KK <102194271+ikhela-04KK@users.noreply.github.com>
2023-07-18 02:00:11 +0200

    Create main_17.pas
    d

---
+++ main_17.pas
@@ -0,0 +1,52 @@
+// ecrire un programme en Pascal qui permet de saisir une liste de personnes avec leur nom et leur âge, puis de trier cette liste par ordre croissant d'âge =. Afficher ensuite la liste triée. 
+
+program triePlusAge;
+uses crt; 
+
+type 
+    person = record
+    nom : string; 
+    age: integer; 
+end; 
+
+perso = array of person;
+ // faites comme ça pour pouvoir mettre un tableau dans une fonction
+//  creation d'une fonction qui enregistre les noms au préalable et qui arrête l'enregistrement lorsqu'on appui sur 0; la fonction doit retourner le tableau pour fonctionner
+// persons : array of person, est ce qu'in peut implementer la un tableau directement dans la fonction 
+
+function savPers(nbPers:integer):perso; 
+    var
+        persons:perso;
+        i:integer;  
+    begin 
+        setLength(persons,nbPers);
+
+        for i:= 1 to length(persons) do 
+            begin
+                WriteLn('entrer personne ', i);
+                ReadLn(persons[i].nom);
+                WriteLn('vous avez entrez le nom de ', persons[i].nom, ' entrez maintenant son age ');
+                ReadLn(persons[i].age);
+            end;
+        savPers := persons;
+    end;
+
+procedure affPers(var personne:perso);
+    var 
+        i:integer; 
+    begin 
+        for i:= 1 to Length(personne) do 
+            writeln('inscrit ', i, '| nom: ', personne[i].nom, 'age: ', personne[i].age);
+    end;
+
+var 
+    nbPers: integer;
+    personnes:perso; 
+
+begin 
+    WriteLn('entrer le nombre de personnes que vous voulez enregistrez ?');
+    ReadLn(nbPers);
+    personnes := savPers(nbPers);
+    affPers(personnes); 
+end.
+
